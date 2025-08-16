716e0f5f6dbb9e0221c3d460bb25fe8553f943bf
Nilsudene <n.kraschon1@gmail.com>
2025-08-16 23:58:30 +0200

    folder for min jae and this days work

--- run3c_ls_conv.py
+++ run3c_ls_conv.py
@@ -14,40 +14,42 @@ from run2_surrogate import surrogates
 # Run line-searches between PES combinations
 lsis = {}
 M = 9
-epsilon_p = [[0.02, 0.02], [0.015, 0.015], [0.01, 0.01], [0.005, 0.005]]
-for xc_srg, pes_srg in pes_dict.items():
-    lsis[xc_srg] = {}
-    xc_ls = 'b3lyp'
-    pes_ls = pes_dict[xc_ls]
-    structure = surrogates[xc_srg].structure.copy()
-    if xc_srg == xc_ls:
-        structure.shift_params([0.1, -0.1]) # only shifting if surrogate is the same as linesearch method
-    # end if
-    for eps in epsilon_p:
-        eps_str = ''.join(f'_{int(e * 1000):03d}' for e in eps)
-        path = f'ls_conv_m{M}/{xc_srg}-{xc_ls}-{eps_str}'
+epsilon_p = [[0.005, 0.005]]
 
-        lsis[xc_srg][eps_str] = {}
-        surrogates[xc_srg].optimize(epsilon_p=eps)
-        
-        lsi = LineSearchIteration(
-            surrogate=surrogates[xc_srg],
-            structure=structure,
-            path=path,
-            pes=pes_ls,
-            M = M,
-        )
-        for i in range(4):
-            lsi.propagate(i, add_sigma=False)
-        # end for
-        # Evaluate the latest eqm structure
-        for i in range(5):
-            lsi.pls(i).evaluate_eqm(add_sigma=False)
-        print(f'Line-search ({xc_ls}) on {xc_srg} surrogate with {eps_str} epsilons:')
-        print(lsi)
-        print(surrogates[xc_ls].structure.params)
-        print('^^Reference params^^')
-        lsis[xc_srg][eps_str][f'{M}'] = lsi
+xc_srg = 'rhf'
+pes_srg = pes_dict[xc_srg]
+lsis[xc_srg] = {}
+xc_ls = 'b3lyp'
+pes_ls = pes_dict[xc_ls]
+structure = surrogates[xc_srg].structure.copy()
+if xc_srg == xc_ls:
+    structure.shift_params([0.1, -0.1]) # only shifting if surrogate is the same as linesearch method
+# end if
+for eps in epsilon_p:
+    eps_str = ''.join(f'_{int(e * 1000):03d}' for e in eps)
+    path = f'ls_conv_m_noise/{M}-{xc_srg}-{xc_ls}-{eps_str}'
+
+    lsis[xc_srg][eps_str] = {}
+    surrogates[xc_srg].optimize(epsilon_p=eps)
+    
+    lsi = LineSearchIteration(
+        surrogate=surrogates[xc_srg],
+        structure=structure,
+        path=path,
+        pes=pes_ls,
+        M = M,
+    )
+    for i in range(4):
+        lsi.propagate(i, add_sigma=True)
     # end for
+    # Evaluate the latest eqm structure
+    for i in range(5):
+        lsi.pls(i).evaluate_eqm(add_sigma=True)
+    print(f'Line-search ({xc_ls} + noise) on {xc_srg} surrogate with {eps_str} epsilons:')
+    print(lsi)
+    print(surrogates[xc_ls].structure.params)
+    print('^^Reference params^^')
+    lsis[xc_srg][eps_str][f'{M}'] = lsi
+# end for
 # end for
 
