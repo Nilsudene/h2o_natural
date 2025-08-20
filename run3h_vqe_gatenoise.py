d7a858834210a0dfe7288d13f727261072a52caf
Nilsudene <n.kraschon1@gmail.com>
2025-08-20 09:22:16 +0200

    one more run of the surrogate code with gatenoise

--- run3h_vqe_gatenoise.py
+++ run3h_vqe_gatenoise.py
@@ -12,37 +12,40 @@ from run2_surrogate import surrogates
 pars = []
 # Run line-searches between PES combinations
 lsis = {}
-p_dep = [1e-6, 1e-5,1e-4,1e-3]
-epsilon_p = [0.001, 0.001]
+p_dep = [1e-3]
+epsilon_p = [0.005, 0.005]
+shots = [1e6]
 xc_srg = 'b3lyp'
 xc_ls = 'VQE'
 structure = surrogates[xc_srg].structure.copy()
 # end if
 for p in p_dep:
     lsis[p] = {}
-    pes_ls = vqe_pes_function(p_dep=p)
-    path = f'ls_vqe_gatenoise/p_dep:{p}'
+    for shot_count in shots:
+        pes_ls = vqe_pes_function(p_dep=p, optimizer='COBYLA', shots = shot_count, trials =3)
+        path = f'ls_vqe_gatenoise/p_dep:{p}'
 
-    surrogates[xc_srg].optimize(epsilon_p=epsilon_p)
-    lsi = LineSearchIteration(
-        surrogate=surrogates[xc_srg],
-        structure=structure,
-        path=path,
-        pes=pes_ls,
-    )
-    for i in range(4):
-        lsi.propagate(i, add_sigma=False)
-    # end for
-    # Evaluate the latest eqm structure
-    lsi.pls().evaluate_eqm(add_sigma=False)
-    print(f'Line-search ({xc_ls}) on B3LYP surrogate with {p} depolarizing noise on cx gates:')
-    print(lsi)
-    lsis[p] = lsi
+        surrogates[xc_srg].optimize(epsilon_p=epsilon_p)
+        lsi = LineSearchIteration(
+            surrogate=surrogates[xc_srg],
+            structure=structure,
+            path=path,
+            pes=pes_ls,
+        )
+        for i in range(3):
+            lsi.propagate(i, add_sigma=False)
+        # end for
+        # Evaluate the latest eqm structure
+        lsi.pls().evaluate_eqm(add_sigma=False)
+        print(f'Line-search ({xc_ls}) on B3LYP surrogate with {p} depolarizing noise on cx gates and :')
+        print(lsi)
+        print(ref_params)
+        print('^^Reference params^^')
+        lsis[p][shot_count] = lsi
 # end for
 
 if __name__ == '__main__':
     to_deg = 180 / np.pi
-    os.makedirs('figures_vqe', exist_ok=True)
 
     pars = []
 
@@ -84,9 +87,10 @@ if __name__ == '__main__':
 
         pars.append([params[-1, 0], params[-1, 1]])
 
+        os.makedirs('figures_vqe_gatenoise', exist_ok =True)
         fig_name = f'vqe_convergence_pdep_{p}.png'
         plt.tight_layout()
-        plt.savefig(f'figures_vqe/{fig_name}', dpi=300)
+        plt.savefig(f'figures_vqe_gatenoise/{fig_name}', dpi=300)
         print(f'Saved figure: {fig_name}')
         plt.close(fig)
 
